92b132d3b00fd6e5b0b118a8cdb0078597630262
Estelle Garcia <estelle.garcia@livafortis.us>
2022-10-17 13:41:04 -0500

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Estelle Garcia on 10/17/22.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
